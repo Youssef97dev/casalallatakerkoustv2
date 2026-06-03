d815fb16d40688a6400ea02a3345775cfd1b75e5
Youssef97dev <youssefmoussafi97@gmail.com>
2026-06-03 13:48:24 +0100

    add contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Loader from "./components/Loader";
+
+export const metadata = {
+  title: "Contact & Reservations | Casa Lalla | Ibiza Style & Agafay Vibes",
+  description:
+    "Contact Casa Lalla Takerkoust to book your table. Experience Mykonos energy and Ibiza style sunsets at one of the top Moroccan restaurants near the lake.",
+};
+
+const page = () => {
+  return <Loader />;
+};
+
+export default page;
